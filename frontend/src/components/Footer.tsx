daf99506a360353d763594537f9ee28c53f65a77
Vlasta Vašut <vasutvlastimil@gmail.com>
2026-07-17 12:20:22 +0200

    moved  the footer to be in center for now

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHub } from "../icons/GitHub"
 
 export const Footer = ()=>{
     return(
-        <footer className="bg-white h-20 text-black flex flex-row  justify-start items-center px-5">
+        <footer className="bg-white h-20 text-black flex flex-row  justify-center items-center px-5">
           <a href="https://github.com/iVlastart/challenge_seeds" target="_blank" className="flex gap-x-2 items-center">
             <GitHub/> Repo
           </a>
