ffe7ff4c2f2f3438f2bbf1e9e9f41a00d7b9532b
Vlasta Vašut <vasutvlastimil@gmail.com>
2026-07-29 12:44:43 +0200

    Just so it does not change width much on the date type change

--- frontend/src/components/SeedTHeader.tsx
+++ frontend/src/components/SeedTHeader.tsx
@@ -7,7 +7,7 @@ export const SeedTHeader = ({isAscending, setIsAscending}:ISeedTHeader)=>{
     return(
         <>
             {tHeader.map((header,key)=>(
-              <th key={key} className={`${header==='date'?'cursor-pointer':''} no-copy`} onClick={header === 'date' ? () => setIsAscending(prev => !prev) : undefined}>
+              <th key={key} className={`${header==='date'?'cursor-pointer min-w-81.25':''} no-copy`} onClick={header === 'date' ? () => setIsAscending(prev => !prev) : undefined}>
                 <div className="flex items-center justify-center gap-1">
                   {String(header).charAt(0).toUpperCase() + String(header).slice(1)}
                   {header === 'date' && (
