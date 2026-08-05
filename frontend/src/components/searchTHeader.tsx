a523799b91bab364f9830cedfcc6ac5d91179978
Vlasta Vašut <vasutvlastimil@gmail.com>
2026-08-05 13:06:52 +0200

    fixed an issue when the selects were not displaying text properly when in dark mode

--- frontend/src/components/searchTHeader.tsx
+++ frontend/src/components/searchTHeader.tsx
@@ -8,7 +8,7 @@ export const SearchTHeader = ({setDateC, setDiffC, setChallengeC}:ISearchTHeader
     return(
         <>
             <th>
-              <select name="dateCategory" className="no-copy" id="dateCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setDateC(e.target.value)}>
+              <select name="dateCategory" className="no-copy bg-base-100" id="dateCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setDateC(e.target.value)}>
                 {dateCategory.map((dc, key) => (
                   <option value={dc} key={key}>
                     {firstUpperCase(dc)}
@@ -17,7 +17,7 @@ export const SearchTHeader = ({setDateC, setDiffC, setChallengeC}:ISearchTHeader
               </select>
             </th>
             <th>
-              <select name="diffCategory" className="no-copy" id="diffCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setDiffC(e.target.value)}>
+              <select name="diffCategory" className="no-copy bg-base-100" id="diffCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setDiffC(e.target.value)}>
                 {diffCategory.map((dc, key) => (
                   <option value={dc} key={key}>
                     {firstUpperCase(dc)}
@@ -26,7 +26,7 @@ export const SearchTHeader = ({setDateC, setDiffC, setChallengeC}:ISearchTHeader
               </select>
             </th>
             <th>
-              <select name="challengeCategory" className="no-copy" id="challengeCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setChallengeC(e.target.value)}>
+              <select name="challengeCategory" className="no-copy bg-base-100" id="challengeCategory" onChange={(e:ChangeEvent<HTMLSelectElement>) => setChallengeC(e.target.value)}>
                 {challengeCategory.map((cc, key) => (
                   <option value={cc} key={key}>
                     {firstUpperCase(cc)}
